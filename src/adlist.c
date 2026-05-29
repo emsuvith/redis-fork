b366a52c082aebca1bf0529a4c38a3e922065da5
Suvith ekdev <160001198+emsuvith@users.noreply.github.com>
2026-05-29 12:14:13 +0530

    Comment out list initialization in adlist.c
    Commented out initialization code for the list structure.

--- src/adlist.c
+++ src/adlist.c
@@ -23,11 +23,11 @@ list *listCreate(void)
     struct list *list;
 
     if ((list = zmalloc(sizeof(*list))) == NULL)
-        return NULL;
-    list->head = list->tail = NULL;
-    list->len = 0;
-    list->dup = NULL;
-    list->free = NULL;
+    //     return NULL;
+    // list->head = list->tail = NULL;
+    // list->len = 0;
+    // list->dup = NULL;
+    // list->free = NULL;
     list->match = NULL;
     return list;
 }
