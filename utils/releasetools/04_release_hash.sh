7b95bd6f310f61995042d7e2421bf78244258bee
Suvith ekdev <160001198+emsuvith@users.noreply.github.com>
2026-06-05 09:51:56 +0530

    Fix echo command to write to README file

--- utils/releasetools/04_release_hash.sh
+++ utils/releasetools/04_release_hash.sh
@@ -7,7 +7,7 @@ fi
 
 SHA=$(curl -s http://download.redis.io/releases/redis-${1}.tar.gz | shasum -a 256 | cut -f 1 -d' ')
 # ENTRY="hash redis-${1}.tar.gz sha256 $SHA http://download.red"
-echo $ENTRY >> ../redis-hashes/README
+echo $ENTRY >> ../redi
 echo "Press any key to commit, Ctrl-C to abort)."
 read yes
 (cd ../redis-hashes; git commit -a -m "${1} hash."; git push)
